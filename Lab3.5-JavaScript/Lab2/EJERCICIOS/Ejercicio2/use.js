3fc1ace1430fc7bbcec3dc04a31bc364b309df18
Arturo-Chi <iachimut101@gmail.com>
2026-02-09 12:22:41 -0600

    purge: aditional folders

--- Lab3.5-JavaScript/Lab2/EJERCICIOS/Ejercicio2/use.js
+++
@@ -1,9 +0,0 @@
-import { digitalClock, alarm } from "./reloj.js";
-
-const d = document;
-
-
-d.addEventListener("DOMContentLoaded", (e)=>{
-    digitalClock(".reloj", "#iniciar-reloj", "#detener-reloj");
-    alarm("#iniciar-alarma", "#detener-alarma")
-});
