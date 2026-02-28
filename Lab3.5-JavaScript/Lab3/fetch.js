a48bb0225be4cb7008589c561a16df4b6e1633b2
Arturo-Chi <iachimut101@gmail.com>
2026-02-28 00:25:56 -0600

    feat: fetch crud end

--- Lab3.5-JavaScript/Lab3/fetch.js
+++ Lab3.5-JavaScript/Lab3/fetch.js
@@ -14,28 +14,38 @@ const url = "http://localhost:3000/penitentes"
 //opciones de método, cors, etc
 
 function editEvents(btns){
-    btns.forEach(b => {
-        b.addEventListener("click", e=>{
-            alert("editando")
-        });
+   btns.forEach(el => {
+        el.addEventListener("click", e=>{
+            d.querySelector('input[name="nombre"]').value = el.dataset.nombre
+            d.querySelector('input[name="id"]').value = el.dataset.id
+        })
     });
 }
 function deleteEvents(btns){
-    btns.forEach(b => {
-        b.addEventListener("click", e=>{
-            alert("borrar")
-        });
+   btns.forEach(el => {
+        el.addEventListener("click", async e=>{
+            console.log(el.dataset.id)
+             const options = {
+                    method:"DELETE",
+                    headers: {
+                        "Content-type": "application/json; charset=utf-8"
+                    },
+                    body:null
+            }
+            let res = await fetch(`${url}/${el.dataset.id}`, options),
+            json = await res.json()
+            console.log(json)
+        })
     });
 }
 
 
 const loadTable = (json) =>{
     json.forEach(el => {
+        console.log(el.nombre, el.id)
         $template.querySelector(".id").textContent = el.id
         $template.querySelector(".nombre").textContent = el.nombre
         
-        
-        //carga de atributos data
         $template.querySelector(".edit").dataset.id = el.id
         $template.querySelector(".edit").dataset.nombre = el.nombre
 
@@ -52,33 +62,77 @@ const loadTable = (json) =>{
 
 }
 
-function fecthingTime(){
-
-}
-
 
 const getAll = async () => {
     //alert("Hola mundo")
     try {
         let response = await fetch(url)
         let json = await response.json();
-
-
+        
         //Si vamos a usar un dato que usa await entonces en sus demás implementaciones dentro del código también se le debe de poner await
-
         if(!response.ok) throw {
             status: response.status,
             statusText: response.statusText
         }
-    
+        
+        nextId = Math.max(...json.map(r => r.id))+1;
+        d.querySelector('input[name="nextid"]').value = nextId
         console.log(json)
         loadTable(json)
 
     } catch (error) {
         let message =  error.statusText || "Ocurrió un error"
-        $table.insertAdjacentHTML("afterend",`<p><b>Error ${error.status} :${message}</b></p>`)
+        $table.insertAdjacentHTML("afterend",`<p><b>Error ${error.status} :${error}</b></p>`)
     }
 }
 
 
 d.addEventListener("DOMContentLoaded", getAll);
+
+//Operaciones de POST, PUT y DELETE
+//Para PUT y DELETE usaramos delegación de eventos en el formulario
+
+d.addEventListener("submit", async e =>{
+    if(e.target === $form){
+        e.preventDefault()//Metodo de autoprocesamiento
+        if(!e.target.id.value){
+            //POST
+
+            try {
+                //Esta vez si va a a llevar opciones
+                const options = {
+                    method:"POST",
+                    headers: {
+                        "Content-type": "application/json; charset=utf-8"
+                    },
+                    body:JSON.stringify({
+                        nombre: e.target.nombre.value,
+                        id: e.target.nextid.value
+                    })
+                }
+                let response = await fetch(url, options)
+                json = await response.json()
+            } catch (error) {
+                console.error(error)
+            }
+
+        }else{
+            //PUT
+            const options = {
+                    method:"PUT",
+                    headers: {
+                        "Content-type": "application/json; charset=utf-8"
+                    },
+                    body:JSON.stringify({
+                        nombre: e.target.nombre.value
+                    })
+            }
+            let res = await fetch(`${url}/${e.target.id.value}`, options),
+            json = await res.json()
+            console.log(json)
+
+        }
+
+
+    }
+});
